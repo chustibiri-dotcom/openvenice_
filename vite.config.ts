a04b00193ddf80fdaaf3a05ac0086dd748b6dd65
chustibiri-dotcom <chustibiri@gmail.com>
2026-03-15 16:21:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/venice': {
-        target: 'https://api.venice.ai',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/venice/, ''),
-      },
-    },
-  },
+  base: '/openvenice_/',
+  plugins: [react()],
 })
